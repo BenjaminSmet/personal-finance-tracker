914cd2622106466d0390bfe1b24d0cefacc5b8e7
BenjaminSmet <benjamin.smet29@gmail.com>
2026-06-19 22:48:28 +0200

    will this work?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/personal-finance-tracker/', // change to your GitHub repo name
+  base: '/personal-finance-tracker/',
   build: {
     outDir: 'dist'
   }
